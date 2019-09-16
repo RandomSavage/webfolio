3668931f88ef4b2184d129982d6b640cd482b262
RandomSavage <randomsavage77@gmail.com>
2019-09-16 14:00:27 -0400

    update

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const clickedBtn = ()=> {
   mobileMenu[0].classList.toggle('active');
 }
 
-menuBtn[0].addEventListener('click', clickedBtn);
+//menuBtn[0].addEventListener('click', clickedBtn);
 
 console.log(menuBtn[0]);
 
