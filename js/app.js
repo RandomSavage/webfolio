2e8b64c671f3095b8e062cc9dc5685ec708d0b27
RandomSavage <randomsavage77@gmail.com>
2020-01-19 19:33:53 -0500

    mobile menu repaired

--- js/app.js
+++ js/app.js
@@ -11,7 +11,30 @@ menuBtn[0].addEventListener('click', clickedBtn);
 console.log(mobileMenu[0]);*/
 
 
-const menuBtn = document.querySelector('.menu-btn');
-const mobileMenu = document.querySelector('.mobile-menu');
+// const menuBtn = document.querySelector('.menu-btn');
+// const mobileMenu = document.querySelector('.mobile-menu');
+//
+// menuBtn.onclick = () => mobileMenu.classList.toggle('active');
+//
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
 
-menuBtn.onclick = () => mobileMenu.classList.toggle('active');
+const myFunction = ()=> {
+  let menuBtn = document.querySelector('.menu-btn');
+  let mobileMenu = document.querySelector('.mobile-menu');
+  if(mobileMenu.style.display = "none") {
+    mobileMenu.classList.toggle('active');
+    mobileMenu.style.display = "flex";
+  } else {
+    console.log("active");
+  }
+}
+
+
+// mobileMenu.style.transform = "translate3d(-100%,0,0)"
